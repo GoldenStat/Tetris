f333dfaa8d439a51c14094570aa9f8b94d46b501
Alexander Völz <alexander.voelz@googlemail.com>
2017-08-07 12:55:47 +0200

    Steuerung Kollision abfrage noch in Arbeit

--- Tetris/GameScene.swift
+++ Tetris/GameScene.swift
@@ -13,7 +13,7 @@ class GameScene: SKScene {
 	
 	var movingTile: TetrisNode!
 	var elapsedTime: Int = 0
-	let timeCycle = 2
+	let timeCycle = 20
 	let grid = TetrisGrid()
 	var cycles = 0
 	
@@ -45,75 +45,123 @@ class GameScene: SKScene {
 		movingTile = tetris
 	}
 	
-	fileprivate func deleteTetrisTile(_ tetrisTile: TetrisNode) {
-		let blocks = tetrisTile.children
+	fileprivate func deleteTetrisTile() {
+		let blocks = movingTile.children
 		for block in blocks {
 			block.move(toParent: self)
 		}
-		tetrisTile.removeFromParent()
+		movingTile.removeFromParent()
 	}
 	
-	func move(movingTile: TetrisNode) {
+	func move() {
 		movingTile.position.y -= grid.dy // move Tetris Tile Down
 	}
 	
+	var lastTouchedLocation : CGPoint?
 	
-    func touchDown(atPoint pos : CGPoint) {
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-	}
-    
-    func touchUp(atPoint pos : CGPoint) {
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-		
+		if let location = touches.first?.location(in: self) {
+			lastTouchedLocation = location
+		}
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		if let location = touches.first?.location(in: self) {
+			if let lastPos = lastTouchedLocation {
+				
+				let dx = CGFloat(Int((location.x - lastPos.x) / grid.dx)) * grid.dx
+//				let dy = CGFloat(Int((location.y - lastPos.y) / grid.dy)) * grid.dy
+				let newPos = movingTile.position.applying(CGAffineTransform(translationX: dx, y: 0))
+				
+				if dx > 0 && !collisionWithBlock(inDir: .right) {
+						movingTile.position = newPos
+				} else if dx < 0 && !collisionWithBlock(inDir: .left) {
+						movingTile.position = newPos
+				}
+				
+				
+				lastTouchedLocation = location
+			}
+		}
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		lastTouchedLocation = nil
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+		lastTouchedLocation = nil
     }
 	
-	func collisionWithBlockOrFrame(tile: TetrisNode) -> Bool {
-		let movingBlocks = tile.children
-		let staleBlocks = self.children.filter({!$0.isEqual(tile)})
+	var staleBlocks : [SKNode] {
+		get { return self.children.filter({!$0.isEqual(movingTile)}) }
+	}
+	
+	func collisionWithBlockOrFrame() -> Bool {
+		return (collisionWithFrame() || collisionWithBlock(inDir: .down))
+	}
+	
+	enum Direction {
+		case down,left,right
+	}
+	
+	func collisionWithBlock(inDir: Direction) -> Bool {
+		let movingBlocks = movingTile.children
 		
 		for block in movingBlocks {
-			let globalPos = tile.convert(block.position, to: self)
-			let blockHitFrame = globalPos.y <= view!.frame.minY + grid.cellHeight
+			let globalPos = movingTile.convert(block.position, to: self)
 			let blockRow = grid.row(from: globalPos)
+			let blockCol = grid.col(from: globalPos)
 			
-			if blockHitFrame {
-				return true
-			} else {
-				for staleBlock in staleBlocks {
-					let staleBlockRow = grid.row(from: staleBlock.position)
-					let blockHitBlock = staleBlockRow + 1 == blockRow && grid.col(from: staleBlock.position) == grid.col(from: globalPos)
-					if blockHitBlock {
-						return blockHitBlock
-					}
+			for staleBlock in staleBlocks {
+				let staleBlockRow = grid.row(from: staleBlock.position)
+				let staleBlockCol = grid.col(from: staleBlock.position)
+				
+				var direction: [Int]
+				
+				switch inDir {
+				case .down:
+					direction = [0,1]
+				case .left:
+					direction = [-1,0]
+				case .right:
+					direction = [1,0]
+				}
+				
+				if staleBlockRow + direction[0] == blockRow && staleBlockCol + direction[1] == blockCol {
+					return true
 				}
 			}
 		}
-		
 		return false
 	}
-    
+	
+	
+	func collisionWithFrame () -> Bool {
+		let movingBlocks = movingTile.children
+		for block in movingBlocks {
+			let globalPos = movingTile.convert(block.position, to: self)
+			let blockHitFrame = globalPos.y <= view!.frame.minY + grid.cellHeight
+			
+			if blockHitFrame {
+				return true
+			}
+			
+		}
+		return false
+	}
+	
+	
     override func update(_ currentTime: TimeInterval) {
 		elapsedTime += 1
 		if elapsedTime > timeCycle {
 			elapsedTime = 0
 			cycles += 1
-			move(movingTile: movingTile)
-			if collisionWithBlockOrFrame(tile: movingTile) {
-				deleteTetrisTile(movingTile)
+			if collisionWithBlockOrFrame() {
+				deleteTetrisTile()
 				createTetris()
+			} else {
+				move()
 			}
 		}
     }
